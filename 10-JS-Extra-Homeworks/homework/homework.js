30baf9c2485589fee39d162f4ca0bd32c4bcc239
GuadaMendezGattari <guadalupemendez.imei@gmail.com>
2022-04-28 19:15:46 -0300

    Extra homework JS not finished

--- 10-JS-Extra-Homeworks/homework/homework.js
+++ 10-JS-Extra-Homeworks/homework/homework.js
@@ -10,6 +10,11 @@ function deObjetoAmatriz(objeto){
       C: 3
     }) ➞ [["D", 1], ["B", 2], ["C", 3]]*/
   //Escribe tu código aquí
+  nuevaMatriz = [];
+  for (clave in objeto) {
+    nuevaMatriz.push([clave, objeto[clave]]);
+  };
+  return nuevaMatriz;
 }
 
 
@@ -18,6 +23,15 @@ function numberOfCharacters(string) {
   //en formato par clave-valor.
   //Ej: Recibe ---> "adsjfdsfsfjsdjfhacabcsbajda" || Devuelve ---> { a: 5, b: 2, c: 2, d: 4, f: 4, h:1, j: 4, s: 5 } 
   //Escribe tu código aquí
+  objetoString = {};
+  for(i = 0; i < string.length; i++) {
+    if(objetoString.hasOwnProperty(string[i])) {
+      objetoString[string[i]] += 1;
+    } else {
+      objetoString[string[i]] = 1;
+    };
+  };
+  return objetoString;
 }
 
 
@@ -26,6 +40,16 @@ function capToFront(s) {
   //al principio de la palabra.
   //Ejemplo: soyHENRY -> HENRYsoy
   //Escribe tu código aquí
+  mayusculas = '';
+  minusculas = '';
+  for(i = 0; i < s.length; i++) {
+    if(s[i] === s[i].toUpperCase()) {
+      mayusculas += s[i];
+    } else {
+      minusculas += s[i];
+    };
+  };
+  return mayusculas + minusculas;
 }
 
 
@@ -43,6 +67,16 @@ function capicua(numero){
   //La misma debe retornar: "Es capicua" si el número se número que se lee igual de 
   //izquierda a derecha que de derecha a izquierda. Caso contrario retorna "No es capicua"
   //Escribe tu código aquí
+  numero = numero.toString();
+  numeroAlReves = '';
+  for (i = numero.length - 1; i >= 0; i--) {
+    numeroAlReves += numero[i];
+  };
+  if (numero === numeroAlReves) {
+    return 'Es capicua';
+  } else {
+    return 'No es capicua';
+  };
 }
 
 
@@ -50,6 +84,15 @@ function deleteAbc(cadena){
   //Define una función que elimine las letras "a", "b" y "c" de la cadena dada 
   //y devuelva la versión modificada o la misma cadena, en caso de contener dichas letras.
   //Escribe tu código aquí
+  cadenaNueva = '';
+  for(i = 0; i < cadena.length; i++) {
+    if (cadena[i] === 'a' || cadena[i] === 'b' || cadena[i] === 'c') {
+      continue;
+    } else {
+      cadenaNueva += cadena[i];
+    };
+  };
+  return cadenaNueva;
 }
 
 
@@ -65,7 +108,18 @@ function buscoInterseccion(arreglo1, arreglo2){
   //retornar un nuevo array con la intersección de ambos elementos. (Ej: [4,2,3] unión [1,3,4] = [3,4].
   //Si no tienen elementos en común, retornar un arreglo vacío.
   //Aclaración: los arreglos no necesariamente tienen la misma longitud
-  //Escribe tu código aquí  
+  //Escribe tu código aquí
+  interseccion = [];
+  for (i = 0; i < arreglo1.length; i++) {
+    for (j = 0; j < arreglo2.length; j++) {
+      if (arreglo2[j] === arreglo1[i]) {
+        interseccion.push(arreglo2[j]);
+      } else {
+        continue;
+      }
+    };
+  };
+  return interseccion;
 }
 
 
